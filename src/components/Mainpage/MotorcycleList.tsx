eca0f96e7389d3232a28e2352ca938cedee59247
shashank7753 <89301898+shashank7753@users.noreply.github.com>
2024-12-27 23:34:24 +0530

    correction on fetch

--- src/components/Mainpage/MotorcycleList.tsx
+++ src/components/Mainpage/MotorcycleList.tsx
@@ -1,39 +1,3 @@
-// import  { useEffect, useState } from 'react';
-// import { MotorcycleCard } from './MotorcycleCard';
-
-// const MotorcycleList = () => {
-//   interface Motorcycle {
-//     id: string;
-//     name: string;
-//     image: string;
-//     price: number;
-//     type: string;
-//     cc: number;
-//     available: boolean;
-//     rating: number;
-//   }
-
-//   const [motorcycles, setMotorcycles] = useState<Motorcycle[]>([]);
-
-//   useEffect(() => {
-//     fetch('/api/motorcycles')
-//       .then((response) => response.json())
-//       .then((data) => setMotorcycles(data))
-//       .catch((error) => console.error('Error fetching motorcycles:', error));
-//   }, []);
-
-//   return (
-//     <div>
-//       {motorcycles.map((motorcycle) => (
-//         <MotorcycleCard key={motorcycle.id} motorcycle={motorcycle} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default MotorcycleList;
-
-
 import { useEffect, useState } from 'react';
 
 interface Motorcycle {
