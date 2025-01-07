7d7c6f0365df6b7b1202c21c65d2e39b600cdaf3
shashank7753 <89301898+shashank7753@users.noreply.github.com>
2025-01-07 15:32:41 +0530

    userjs.update

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,25 +1,25 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../config/database');
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  walletBalance: {
-    type: DataTypes.FLOAT,
-    defaultValue: 0,
-  },
-});
+// const User = sequelize.define('User', {
+//   id: {
+//     type: DataTypes.UUID,
+//     defaultValue: DataTypes.UUIDV4,
+//     primaryKey: true,
+//   },
+//   email: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     unique: true,
+//   },
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   walletBalance: {
+//     type: DataTypes.FLOAT,
+//     defaultValue: 0,
+//   },
+// });
 
-module.exports = User;
+// module.exports = User;
